3eb9bb73a217a60741afeb49c54bc8ef271d0c3d
algerkong <1455048564@qq.com>
2020-08-15 16:55:52 +0800

    打包dist

--- src/common/const.js
+++ src/common/const.js
@@ -1,5 +1,5 @@
-const baseUrl = 'http://localhost:3000'
+// const baseUrl = 'http://localhost:3000'
 // const baseUrl = 'https://autumnfish.cn'
-// const baseUrl = 'http://124.70.158.250:3000/'
+const baseUrl = 'http://124.70.158.250:3000/'
 
 export default baseUrl
